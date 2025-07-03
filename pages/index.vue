14cc73da48ccc7f778a52e199c3879d2eadd9b06
Alex Berger <abe@unifiedarts.de>
2025-07-03 16:24:48 +0200

    2: remove circle, will add something else

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,9 @@
 <template>
     <div class="relative w-full h-dvh overflow-hidden">
-        <div
-            class="absolute left-1/2 top-1/2 -translate-1/2 flex justify-center items-center relative
-            w-[80%] max-w-[650px] aspect-square rounded-full overflow-hidden z-20 shadow-[0px_0px_10px_7px_var(--realm-primary-color)] transition-shadow duration-500">
-            <div class="relative text-black z-20 bg-white rounded-full w-1/2 h-1/2 flex justify-center items-center overflow-hidden text-center">
-                <Transition name="fade" mode="out-in">
-                    <div v-show="isRealmActive('about')" class="absolute w-full h-full flex flex-col justify-center items-center">
-                        <h1 class="text-3xl">Alex "Mugentoki" Berger</h1>
-                        <p>Some stuff about myself</p>
-                    </div>
-                </Transition>
-                <Transition name="fade" mode="out-in">
-                    <div v-show="isRealmActive('web')" class="absolute w-full h-full flex flex-col justify-center items-center">
-                        <h1 class="text-3xl">Web Projects</h1>
-                        <p>Super cool web projects</p>
-                    </div>
-                </Transition>
-                <Transition name="fade" mode="out-in">
-                    <div v-show="isRealmActive('astronomy')" class="absolute w-full h-full flex flex-col justify-center items-center">
-                        <h1 class="text-3xl">Astronomy</h1>
-                        <p>Spacy Astronomy projects</p>
-                    </div>
-                </Transition>
-                <Transition name="fade" mode="out-in">
-                    <div v-show="isRealmActive('worldbuilding')" class="absolute w-full h-full flex flex-col justify-center items-center">
-                        <h1 class="text-3xl">Worldbuilding</h1>
-                        <p>Fantastic fantasy world building projects</p>
-                    </div>
-                </Transition>
-                <Transition name="fade" mode="out-in">
-                    <div v-show="isRealmActive('fractal')" class="absolute w-full h-full flex flex-col justify-center items-center">
-                        <h1 class="text-3xl">Fractal Drift</h1>
-                        <p>Feel the randomness</p>
-                    </div>
-                </Transition>
-            </div>
+        <div class="absolute z-20">
             <a
                 id="about"
-                class="cake-slice transition-bg duration-500"
+                class="transition-bg duration-500"
                 :class="isRealmActive('about') ? 'bg-black/75': 'bg-orange-700'"
                 @mouseenter="setRealm('about')"
                 @focus="setRealm('about')"
@@ -46,7 +12,7 @@
             </a>
             <a
                 id="web"
-                class="cake-slice bg-red-800 rotate-[72deg] transition-bg duration-500"
+                class="bg-red-800 transition-bg duration-500"
                 :class="isRealmActive('web') ? 'bg-transparent': ''"
                 @mouseenter="setRealm('web')"
                 @focus="setRealm('web')"
@@ -55,7 +21,7 @@
             </a>
             <a
                 id="astronomy"
-                class="cake-slice bg-blue-800 rotate-[144deg] transition-bg duration-500"
+                class="bg-blue-800 transition-bg duration-500"
                 :class="isRealmActive('astronomy') ? 'bg-transparent': ''"
                 @mouseenter="setRealm('astronomy')"
                 @focus="setRealm('astronomy')"
@@ -64,7 +30,7 @@
             </a>
             <a
                 id="worldbuilding"
-                class="cake-slice bg-orange-800 rotate-[216deg] transition-bg duration-500"
+                class="bg-orange-800 transition-bg duration-500"
                 :class="isRealmActive('worldbuilding') ? 'bg-transparent': ''"
                 @mouseenter="setRealm('worldbuilding')"
                 @focus="setRealm('worldbuilding')"
@@ -73,13 +39,50 @@
             </a>
             <a
                 id="fractal"
-                class="cake-slice bg-purple-800 rotate-[288deg] transition-bg duration-500"
+                class="bg-purple-800 transition-bg duration-500"
                 :class="isRealmActive('fractal') ? 'bg-transparent': ''"
                 @mouseenter="setRealm('fractal')"
                 @focus="setRealm('fractal')"
             >
                 <div class="w-full h-full flex justify-center items-end pb-8">Fractal Drift</div>
             </a>
+        </div>
+
+        <div
+            class="left-1/2 top-1/2 -translate-1/2 flex justify-center items-center relative
+            w-[80%] max-w-[650px] aspect-square rounded-full overflow-hidden z-20 shadow-[0px_0px_10px_7px_var(--realm-primary-color)] transition-shadow duration-500">
+            <div class="relative text-black z-30 bg-white/80 rounded-full w-full h-full flex justify-center items-center overflow-hidden text-center">
+                <Transition name="fade" mode="out-in">
+                    <div v-show="isRealmActive('about')" class="absolute w-full h-full flex flex-col justify-center items-center">
+                        <h1 class="text-3xl">Mugentoki</h1>
+                        <p>Some stuff about myself</p>
+                    </div>
+                </Transition>
+                <Transition name="fade" mode="out-in">
+                    <div v-show="isRealmActive('web')" class="absolute w-full h-full flex flex-col justify-center items-center">
+                        <h2 class="text-3xl">Web Projects</h2>
+                        <p>Super cool web projects</p>
+                    </div>
+                </Transition>
+                <Transition name="fade" mode="out-in">
+                    <div v-show="isRealmActive('astronomy')" class="absolute w-full h-full flex flex-col justify-center items-center">
+                        <h2 class="text-3xl">Astronomy</h2>
+                        <p>Spacy Astronomy projects</p>
+                    </div>
+                </Transition>
+                <Transition name="fade" mode="out-in">
+                    <div v-show="isRealmActive('worldbuilding')" class="absolute w-full h-full flex flex-col justify-center items-center">
+                        <h2 class="text-3xl">Worldbuilding</h2>
+                        <p>Fantastic fantasy world building projects</p>
+                    </div>
+                </Transition>
+                <Transition name="fade" mode="out-in">
+                    <div v-show="isRealmActive('fractal')" class="absolute w-full h-full flex flex-col justify-center items-center">
+                        <h2 class="text-3xl">Fractal Drift</h2>
+                        <p>Feel the randomness</p>
+                    </div>
+                </Transition>
+            </div>
         </div>
         <div class="absolute top-0 left-0 w-full h-full z-10">
             <!-- Todo: need to replace images with own ones. For now it's just placeholders -->
